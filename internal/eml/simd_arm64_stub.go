e50a6239a29899f9538a466de1a8c9e2b789dc00
23skdu <23skdu@users.noreply.github.com>
2026-04-30 11:18:51 -0700

    Fix ARM64 stubs and cleanup scratch files

--- internal/eml/simd_arm64_stub.go
+++ internal/eml/simd_arm64_stub.go
@@ -7,7 +7,7 @@ func addNEON(_, _, _ []float64)       {}
 func subNEON(_, _, _ []float64)       {}
 func mulNEON(_, _, _ []float64)       {}
 func divNEON(_, _, _ []float64)       {}
-func addScalarNEON(_, _ float64, _ []float64) {}
-func mulScalarNEON(_, _ float64, _ []float64) {}
+func addScalarNEON(_ []float64, _ float64, _ []float64) {}
+func mulScalarNEON(_ []float64, _ float64, _ []float64) {}
 func sqrtNEON(_, _ []float64)         {}
 func detectARM64SIMD()                {}
