65b2ed4518331e565710773c441919a354bbf5ed
23skdu <23skdu@users.noreply.github.com>
2026-06-02 23:46:01 -0700

    Remove unused functions from arm64 stub file to resolve IDE unused warnings

--- internal/eml/simd_arm64_stub.go
+++ internal/eml/simd_arm64_stub.go
@@ -3,23 +3,5 @@
 
 package eml
 
-func addNEON(_, _, _ []float64)       {}
-func subNEON(_, _, _ []float64)       {}
-func mulNEON(_, _, _ []float64)       {}
-func divNEON(_, _, _ []float64)       {}
-func addScalarNEON(_ []float64, _ float64, _ []float64) {}
-func mulScalarNEON(_ []float64, _ float64, _ []float64) {}
-func sqrtNEON(_, _ []float64)         {}
-func detectARM64SIMD()                {}
 func addSVE(_, _, _ []float64)        {}
-func subSVE(_, _, _ []float64)        {}
-func mulSVE(_, _, _ []float64)        {}
-func divSVE(_, _, _ []float64)        {}
-func addScalarSVE(_ []float64, _ float64, _ []float64) {}
-func mulScalarSVE(_ []float64, _ float64, _ []float64) {}
-func sqrtSVE(_, _ []float64)          {}
-func absSVE(_, _ []float64)           {}
-func negSVE(_, _ []float64)           {}
-func invSVE(_, _ []float64)           {}
-func fmaSVE(_, _, _, _ []float64)     {}
-func sveVL() int                      { return 16 }
+
